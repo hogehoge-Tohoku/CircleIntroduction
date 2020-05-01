1af1877dcf6d9c0c0c90058008806d7f1d52b428
TaiseiInoue <t.base.8901@icloud.com>
2020-05-01 13:42:46 +0900

    団体を追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS circle(
     name VARCHAR(50),
     classification VARCHAR(50),/*分類*/
     organization VARCHAR(50),/*所属*/
-    atmosphere VARCHAR(100),/*雰囲気*/
+    atmosphere TEXT,/*雰囲気*/
     introduction TEXT,/*活動内容*/
     welcome_party_inf TEXT,/*新歓情報*/
     email VARCHAR(50),/*お問い合わせ*/
